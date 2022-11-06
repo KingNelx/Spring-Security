3ba118750af1e6b46f7ecc6243773de665fd9f7c
Jonel Tapia <tapiajonel606@gmail.com>
2022-11-06 17:30:35 +0800

    enable web security

--- src/main/java/springsecurity/Config/WebSecurityConfig.java
+++ src/main/java/springsecurity/Config/WebSecurityConfig.java
@@ -1,8 +1,7 @@
 package springsecurity.Config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
-
 }

947a71192f2477acb4489c4870ab39bde7f13216
Jonel Tapia <tapiajonel606@gmail.com>
2022-11-06 17:43:31 +0800

    added BCryptPasswordEncoder

--- src/main/java/springsecurity/Service/UserServiceImpl.java
+++ src/main/java/springsecurity/Service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package springsecurity.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import springsecurity.Entity.User;
 import springsecurity.Model.UserModel;
@@ -11,7 +12,8 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public User registerUser(UserModel userModel) {
         User user = new User();
